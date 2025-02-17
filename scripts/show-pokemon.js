c287a3f7505610b9449dd5df1ab9b1d0d51401cf
FES001 <firdavs7794948@gmail.com>
2025-02-18 01:23:26 +0500

    Working on it.

--- scripts/show-pokemon.js
+++ scripts/show-pokemon.js
@@ -1,10 +1,14 @@
 // POKEMON
+const pokemon = document.querySelector('.pokemon');
 const playPokemonSound = document.querySelector('#playPokemonSound');
 const pokemonImage = document.querySelector('.pokemon-image-itself');
 const pokemonName = document.querySelector('.pokemon-info-name');
 const pokemonAudio = document.querySelector('.pokemon-info-cries-button-audio');
 const pokemonHeight = document.querySelector('.pokemon-info-text-inner-height');
 const pokemonWeight = document.querySelector('.pokemon-info-text-inner-weight');
+
+// POKEMON
+let startPosY = 0, startPosX = 0, newPosY = 0, newPosX = 0;
 
 // RETRIEVING THE DATA FROM LOCAL STORE
 
@@ -45,5 +49,30 @@ function retrivingTheDataFromLocalStore() {
 retrivingTheDataFromLocalStore();
 
 // PLAY THE SOUND
-
 playPokemonSound.addEventListener('click', () => pokemonAudio.play());
+
+// ROTATING THE POKEMON
+pokemon.addEventListener('mousedown', e => {
+    startPosY = e.clientY;
+    startPosX = e.clientX;
+
+    document.addEventListener('mousemove', rotateThePokemonCard);
+    document.addEventListener('mouseup', () => {
+        pokemon.style.transform = `perspective(600px) rotateY(0deg) rotateX(0deg)`;
+        document.removeEventListener('mousemove', rotateThePokemonCard)
+    });
+});
+
+function rotateThePokemonCard(e) {
+    newPosY = startPosY - e.clientY;
+    newPosX = startPosX - e.clientX;
+
+    pokemon.style.transform = `perspective(600px) rotateY(${newPosX}deg) rotateX(${newPosY}deg)`;
+
+    // SETTING BOUDARIES
+    /* if (newPosX < -50) {
+        document.removeEventListener('mousemove', rotateThePokemonCard)
+    } else if (newPosX > 50) {
+        document.removeEventListener('mousemove', rotateThePokemonCard)
+    }; */
+};
